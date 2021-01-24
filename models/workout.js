ac40005acdd292445f7c6d0febdd9afebdbe5e75
Nadine Delaire <ndelaire16@gmail.com>
2021-01-24 09:53:43 -0800

    changes to model

--- models/workout.js
+++ models/workout.js
@@ -10,15 +10,15 @@ const workoutSchema = new Schema({
         {
             type: {
                 type: String,
-                require: true
+               
             },
             name: {
                 type: String,
-                require: true
+              
             },
             duration: {
                 type: Number,
-                require: true
+             
             },
             weight: {
                 type: Number
